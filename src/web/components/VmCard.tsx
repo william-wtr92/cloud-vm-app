38639c4bcb865d9cd626b01a18a8b29fd786fd19
william-wtr92 <wwautrin@hotmail.fr>
2024-02-15 10:17:17 +0100

    chore(init): initial commit

---
+++ src/web/components/VmCard.tsx
@@ -0,0 +1,11 @@
+import styles from "@/styles/components/VmCard.module.css"
+
+const VmCard = () => {
+  return (
+    <div className={styles.vmcardContainer}>
+      <h1>VM Card</h1>
+    </div>
+  )
+}
+
+export default VmCard
